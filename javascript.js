290b22c5ae2acc84da32b343487bc0b05de8b1db
aulline13 <polinapeeva@yandex.ru>
2025-06-25 15:04:09 +0800

    Implement removal of default grid and generation of user grid

--- javascript.js
+++ javascript.js
@@ -1,14 +1,20 @@
 const containerGrid = document.querySelector(".container-grid");
+const defaultNumberOfSquares = 16;
 
-
-function createDivGrid() {
-    for (i = 0; i < 256; i++)
-        containerGrid.appendChild(document.createElement("div")).setAttribute("class", "div-grid");
+function createDivGrid(userInput) {
+    let gridSize = userInput * userInput;
+    let widthHeight = `${512 / userInput}`;
+    for (i = 0; i < gridSize; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.style.width = `${widthHeight}px`; 
+        newDiv.style.height = `${widthHeight}px`;
+        containerGrid.appendChild(newDiv).setAttribute("class", "div-grid");
+    }
 }
 
-createDivGrid();
+createDivGrid(defaultNumberOfSquares);
 
-const divGrid = document.querySelectorAll(".div-grid")
+let divGrid = document.querySelectorAll(".div-grid")
 
 function getRandomRGB() {
     let r = Math.floor(Math.random() * 256); // Random between 0-255
@@ -17,8 +23,31 @@ function getRandomRGB() {
     return `rgb(${r}, ${g}, ${b})`;
 }
 
-divGrid.forEach((div) => {
-    div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = getRandomRGB();
-    })
+function changeColorOnHover() {
+    divGrid.forEach((div) => {
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = getRandomRGB();
+        })
 })
+}
+
+changeColorOnHover()
+
+const newGridButton = document.querySelector(".new-grid-button")
+
+newGridButton.addEventListener("click", () => {
+    let numberOfSquares = prompt("Enter the number of squares (0-100) per side for the new grid:", `${defaultNumberOfSquares}`)
+    numberOfSquares = +numberOfSquares;
+    if (numberOfSquares > 100 || isNaN(numberOfSquares)) {
+        alert("Value entered is greater than 100 or is not a number. Please click the button again to enter a new value.")
+    }
+    else {
+        divGrid.forEach((div) => {
+            containerGrid.removeChild(div)
+        }) 
+        createDivGrid(numberOfSquares);
+        divGrid = document.querySelectorAll(".div-grid");
+        changeColorOnHover();
+    }
+})
+
