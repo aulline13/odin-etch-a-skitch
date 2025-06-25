402f6f64d46545b6ab0f0d0fc39f0bb9dd2dda26
aulline13 <polinapeeva@yandex.ru>
2025-06-25 12:37:03 +0800

    Add change-color-on-hover effect

--- javascript.js
+++ javascript.js
@@ -1,8 +1,24 @@
 const containerGrid = document.querySelector(".container-grid");
 
-function divGrid() {
+
+function createDivGrid() {
     for (i = 0; i < 256; i++)
-        containerGrid.appendChild(document.createElement("div"));
+        containerGrid.appendChild(document.createElement("div")).setAttribute("class", "div-grid");
 }
 
-divGrid();
+createDivGrid();
+
+const divGrid = document.querySelectorAll(".div-grid")
+
+function getRandomRGB() {
+    let r = Math.floor(Math.random() * 256); // Random between 0-255
+    let g = Math.floor(Math.random() * 256); // Random between 0-255
+    let b = Math.floor(Math.random() * 256); // Random between 0-255
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+divGrid.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = getRandomRGB();
+    })
+})
